a8f745c9d9ecc134aa31303332f48861aa452a6b
Consoli <matheus.consoli7@gmail.com>
2018-07-04 22:15:04 -0300

    Htlize and ox-reveal added.

--- config.el
+++ config.el
@@ -15,6 +15,14 @@
     :config (add-hook `org-mode-hook
                     (lambda () (org-bullets-mode 1))))
 
+(use-package ox-reveal
+  :ensure ox-reveal)
+(setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
+(setq org-reveal-mathjax t)
+
+(use-package htmlize
+  :ensure t)
+
 (use-package dashboard
   :ensure t
   :config
