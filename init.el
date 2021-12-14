709dcaba9668265b90621e699b4a0d4724acb9fd
Matheus Consoli <matheus.consoli7@gmail.com>
2021-12-14 20:05:09 -0300

    Improve LSP integration and other things

--- init.el
+++ init.el
@@ -7,22 +7,18 @@
 			             ("org" . "https://orgmode.org/elpa/")))
 (package-initialize)
 
-;; (setq gc-cons-threshold-original gc-cons-threshold)
-;; (setq gc-cons-threshold 100000000)
-;; (add-hook 'after-init-hook (lambda () (setq gc-cons-threshold gc-cons-threshold-original)))
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* (* 1024 1024) 5)) ;; 5mb
 
 (eval-when-compile (require 'use-package))
 
 (setq use-package-always-ensure t)
 
+(setq comp-deferred-compilation t)
+
 
 (eval-when-compile
   (require 'use-package))
-;; (require 'diminish)                ;; if you use :diminish
 (require 'bind-key)                ;; if you use any :bind variant
 
 
@@ -32,6 +28,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("06ed754b259cb54c30c658502f843937ff19f8b53597ac28577ec33bb084fa52" "d9a28a009cda74d1d53b1fbd050f31af7a1a105aa2d53738e9aa2515908cac4c" "11cc65061e0a5410d6489af42f1d0f0478dbd181a9660f81a692ddc5f948bf34" "3c7a784b90f7abebb213869a21e84da462c26a1fda7e5bd0ffebf6ba12dbd041" "e8567ee21a39c68dbf20e40d29a0f6c1c05681935a41e206f142ab83126153ca" default))
  '(package-selected-packages
    '(ob-ipython elpy lsp-python-ms ob-rust rust-mode yaml-mode tree-sitter-langs tree-sitter multiple-cursors highlight-indent-guides format-all auto-highlight-symbol smartparens lsp-ui iedit dap-mode flycheck-inline flycheck yasnippet-snippets yasnippet company-quickhelp company-box company git-gutter-fringe+ fringe-helper doom-modeline rainbow-delimiters all-the-icons which-key use-package undo-tree swiper-helm solaire-mode ox-reveal ox-gfm org-bullets multi-term magit kaolin-themes htmlize helm-rg helm-projectile google-translate good-scroll flyspell-popup flyspell-lazy expand-region easy-kill drag-stuff doom-themes dockerfile-mode crux comment-tags auto-package-update anzu))
  '(warning-suppress-log-types '((emacs) (comp) (comp) (:warning)))
