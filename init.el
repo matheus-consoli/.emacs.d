d9826aa5be874ff81489b9dde265d2a89ef51631
Matheus Consoli <matheus.consoli7@gmail.com>
2022-02-18 19:54:02 -0300

    Change font, theme and add quality of life rust pairs

--- init.el
+++ init.el
@@ -50,3 +50,23 @@
 (require 'straight-x)
 
 (org-babel-load-file (expand-file-name (concat user-emacs-directory "readme.org")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1278c5f263cdb064b5c86ab7aa0a76552082cf0189acf6df17269219ba496053" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ahs-definition-face ((t (:background "dark orange" :foreground "black"))))
+ '(ahs-face ((t (:background "orange" :foreground "black"))))
+ '(ahs-plugin-defalt-face ((t (:background "#1E2029" :foreground "dark orange"))) t)
+ '(company-tooltip-common ((t (:inhirit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inhirit company-tooltip-selection :weight bold :underline nil))))
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch))))))
