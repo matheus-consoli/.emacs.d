1560fa8eb1f97625376558e5ceb65eb1c8ea44af
Consoli <matheus.consoli7@gmail.com>
2022-06-19 22:06:23 -0300

    Font, theme, ordering

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; The default is 800 kilobytes.  Measured in bytes.
-(setq gc-cons-threshold (* 100 1024 1024))
-(setq read-process-output-max (* 100 1024 1024 ))
+(setq gc-cons-threshold (* 200 1024 1024))
+(setq read-process-output-max (* 200 1024 1024 ))
 
 ;; Profile emacs startup
 (add-hook 'emacs-startup-hook
