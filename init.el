9a438a27bbcfaa129fc4ff6e37a4ec0327846d48
Matheus Consoli <matheus.consoli7@gmail.com>
2023-07-18 13:11:55 -0300

    Use centaur-tabs

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; Profile emacs startup
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (message "*** Emacs loaded in %s with %d garbage collections."
+            (message "Emacs loaded in %s with %d garbage collections."
                      (format "%.2f seconds"
                              (float-time
                               (time-subtract after-init-time before-init-time)))
