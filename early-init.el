342b44e4d1d4c4a0e8d14c74ff45dbab45056238
Matheus Consoli <matheus.consoli7@gmail.com>
2024-06-26 22:57:33 -0300

    A bunch of modifications related to treesit

--- early-init.el
+++ early-init.el
@@ -1,4 +1,14 @@
 (setenv "LSP_USE_PLISTS" "true")
 (setq package-enable-at-startup nil)
 
+;; The default is 800 kilobytes.  Measured in bytes.
+;; emacs-lsp performance improvements
+;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+(setq gc-cons-threshold (* 200 1024 1024)) ;; 200mb
+(setq read-process-output-max (* 3 1024 1024 )) ;; 3mb
+
+;; keep the eln cache clean
+(setq native-compile-prune-cache t)
+(setq load-prefer-newer noninteractive)
+
 (provide 'early-init)
