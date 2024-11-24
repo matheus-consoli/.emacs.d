848cbf2c84cb882004d2810a5dc2239c6047f24f
Matheus Consoli <matheus.consoli7@gmail.com>
2024-11-24 10:31:20 -0300

    New changes

--- early-init.el
+++ early-init.el
@@ -5,10 +5,11 @@
 ;; The default is 800 kilobytes.  Measured in bytes.
 ;; emacs-lsp performance improvements
 ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
-(setq gc-cons-threshold (* 200 1024 1024)) ;; 200mb
+(setq gc-cons-threshold (* 200 1024 1024))
+(setq gc-cons-percentage 0.4)
 ;; Optimizations for improving IO performance. Increase max bytes read from a sub-process in a single op
 ;; default value: 3145728 (3M)
-(setq read-process-output-max (* 5 1024 1024 )) ;; 5mb
+(setq read-process-output-max (* 8 1024 1024 )) ;; 8mb
 
 ;; keep the eln cache clean
 (setq load-prefer-newer t)
@@ -19,6 +20,6 @@
 
 (when 'native-comp-compiler-options
   (setq native-comp-speed 2
-        native-comp-compiler-options '("-O3" "-mtune=native")))
+        native-comp-compiler-options '("-O2" "-mtune=native")))
 
 (provide 'early-init)
